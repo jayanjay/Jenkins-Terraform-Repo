24b81d0965d50a76f4885d22ebe3b7eb92bd8bc5
jayanjay <77397453+jayanjay@users.noreply.github.com>
2025-07-26 18:57:03 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,31 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID to use"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags for the instance"
+  type        = map(string)
+  default     = {}
+}
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = "AKIA4O3AOZQ6SWHNYP6P"
+}
+variable "aws_secret_key" {
+  description = "AWS access key"
+  type        = string
+  default     = "Pq3VAiZ5/QcZ1fOW80WE32NkEdU2DThnUPZXTBtr"
+}
